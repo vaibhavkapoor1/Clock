a2af9ae1587827e5140b7ae4d7023f7e42f6a6a4
Vaibhav Kapoor <74494401+vaibhavkapoor1@users.noreply.github.com>
2023-06-21 12:48:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//console.log("Hello world");
+
+
+
+
+let element= document.getElementById('time')
+setInterval(() => {
+    let date=new Date()
+    // let hours= date.getHours()
+    // let minutes=date.getMinutes()
+    // let seconds=date.getSeconds()
+    // console.log(hours,minutes,seconds)
+    
+    let time= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    element.innerHTML = time; 
+    
+},1000)
